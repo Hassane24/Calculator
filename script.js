2c66bf78a6f1971e316614fe0002fc693e73c179
Hassane24 <hsisane5@gmail.com>
2022-01-04 17:33:19 +0100

    WORKS LIKE A CHARM

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-const div_result = document.querySelector(".result");
+let div_result = document.querySelector(".result");
+const numberButtons = document.querySelectorAll(".numbs");
+console.log(numberButtons);
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
 }
@@ -23,7 +25,15 @@ function operate(numberOne, numberTwo) {
   numberTwo = Number(numberTwo);
   if (operation == "+") return console.log(add(numberOne, numberTwo));
   if (operation == "-") return console.log(substract(numberOne, numberTwo));
-  if (operation == "x" || operation == "*") return console.log(multiply(numberOne, numberTwo));
+  if (operation == "x" || operation == "*")
+    return console.log(multiply(numberOne, numberTwo));
   return console.log(devide(numberOne, numberTwo));
 }
+let storage = "";
 
+for (let i = 0; i < numberButtons.length; i++) {
+  numberButtons[i].addEventListener("click", () => {
+    div_result.innerText = i.toString() + storage.toString();
+    storage = div_result.innerText.toString();
+  });
+}
