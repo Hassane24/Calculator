ca2eb75b351f1430309346aedc4a589a1dbb102e
Hassane24 <hsisane5@gmail.com>
2022-01-05 17:33:39 +0100

    WORKS LIKE A CHARM

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let div_result = document.querySelector(".result");
 const numberButtons = document.querySelectorAll(".numbs");
-const plusButton = document.querySelector(".ops-1");
-const minusButton = document.querySelector(".ops-2");
-const multiplyButton = document.querySelector(".ops-3");
-const devideButton = document.querySelector(".ops-4");
+const operators = document.querySelectorAll(".ops");
 
 function add(numberOne, numberTwo) {
   return numberOne + numberTwo;
@@ -22,39 +19,51 @@ function devide(numberOne, numberTwo) {
 }
 
 function operate(numberOne, numberTwo) {
-  const operation = prompt("Which arithmetic operations you want").toString();
-  numberOne = prompt("First number");
-  numberTwo = prompt("Second number");
   numberOne = Number(numberOne);
   numberTwo = Number(numberTwo);
-  if (operation == "+") return console.log(add(numberOne, numberTwo));
-  if (operation == "-") return console.log(substract(numberOne, numberTwo));
-  if (operation == "x" || operation == "*")
-    return console.log(multiply(numberOne, numberTwo));
-  return console.log(devide(numberOne, numberTwo));
+  if (operator == "+") return console.log(add(numberOne, numberTwo));
+  if (operator == "-") return console.log(substract(numberOne, numberTwo));
+  if (operator == "*") return console.log(multiply(numberOne, numberTwo));
+  if (operator == "/") return console.log(devide(numberOne, numberTwo));
 }
 
 let numberOne = "";
+let numberTwo = "";
+let operator = "";
 
-for (let i = 0; i < numberButtons.length; i++) {
-  numberButtons[i].addEventListener("click", () => {
-    div_result.innerText = numberOne.toString() + i.toString();
-    numberOne = div_result.innerText.toString();
-    return numberOne;
+operators.forEach((ops) => {
+  ops.addEventListener("click", (e) => {
+    if (e.target.innerText == "+") {
+      div_result.innerText += "+";
+      operator = "+";
+    }
+    if (e.target.innerText == "-") {
+      div_result.innerText += "-";
+      operator = "-";
+    }
+    if (e.target.innerText == "*") {
+      div_result.innerText += "*";
+      operator = "*";
+    }
+    if (e.target.innerText == "/") {
+      div_result.innerText += "/";
+      operator = "/";
+    }
+    if (e.target.innerText == "=") {
+      operate(numberOne, numberTwo);
+    }
   });
-}
-plusButton.addEventListener("click", () => {
-  div_result.innerText = numberOne + "+";
 });
 
-minusButton.addEventListener("click", () => {
-  div_result.innerText = numberOne + "-";
-});
-
-multiplyButton.addEventListener("click", () => {
-  div_result.innerText = numberOne + "*";
-});
-
-devideButton.addEventListener("click", () => {
-  div_result.innerText = numberOne + "/";
+numberButtons.forEach((numbs) => {
+  numbs.addEventListener("click", (e) => {
+    if (operator === "") {
+      numberOne += e.target.innerText;
+      div_result.innerText = numberOne.toString();
+    } else {
+      numberTwo += e.target.innerText;
+      div_result.innerText =
+        numberOne.toString() + operator + numberTwo.toString();
+    }
+  });
 });
